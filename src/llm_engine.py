f9f0af79b5ed5784e5980d6c71f6ee34733972c7
tanyamishra26 <tanyamishra260@gmail.com>
2026-03-21 15:42:54 +0530

    fixing code

--- src/llm_engine.py
+++ src/llm_engine.py
@@ -1,9 +1,10 @@
 from groq import Groq
+import streamlit as stg
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=st.secrets["GROQ_API_KEY"]
 )
